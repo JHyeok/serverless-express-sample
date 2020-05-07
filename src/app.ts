a620712e944980066695732c35ab2ee98c6ef95c
JHyeok <dev.jhyeok@gmail.com>
2020-05-08 00:27:18 +0900

    Add ESLint and Prettier

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
 import serverless from "serverless-http";
 import express from "express";
-import { APIGatewayProxyEvent, Context, Handler, APIGatewayProxyResult } from "aws-lambda";
+import {
+  APIGatewayProxyEvent,
+  Context,
+  Handler,
+  APIGatewayProxyResult,
+} from "aws-lambda";
 
 const app = express();
 
 app.get("/hello", (req: express.Request, res: express.Response) => {
-  return res.json({message: "Hello"});
+  return res.json({ message: "Hello" });
 });
 
 const serverlessApp = serverless(app);
 
-export const handler: Handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
+export const handler: Handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context,
+): Promise<APIGatewayProxyResult> => {
   const result = await serverlessApp(event, context);
   return result;
 };
